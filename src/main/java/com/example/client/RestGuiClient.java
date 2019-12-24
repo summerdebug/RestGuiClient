303c08615d2552ba5f13ac44eb514617ae9d4f95
“summerdebug” <summerdebug@gmail.com>
2019-12-24 13:47:01 +0300

    Initial commit

---
+++ src/main/java/com/example/client/RestGuiClient.java
@@ -0,0 +1,8 @@
+package com.example.client;
+
+public class RestGuiClient {
+
+    public static void main(String... args) {
+        System.out.println("Start rest gui client.");
+    }
+}
---
+++ src/test/java/com/example/client/RestGuiClientTest.java
@@ -0,0 +1,11 @@
+package com.example.client;
+
+import org.junit.jupiter.api.Test;
+
+class RestGuiClientTest {
+
+    @Test
+    void whenMainThenSuccessful() throws Exception {
+        RestGuiClient.main();
+    }
+}
