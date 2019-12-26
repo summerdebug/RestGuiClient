ef56fa58dd8898f66ea2bfa2a9a411be3de902df
“summerdebug” <summerdebug@gmail.com>
2019-12-26 21:42:09 +0300

    Added simple UI with label and button

--- src/main/java/com/example/client/RestGuiClient.java
+++ src/main/java/com/example/client/RestGuiClient.java
@@ -1,8 +1,36 @@
 package com.example.client;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class RestGuiClient {
 
     public static void main(String... args) {
-        System.out.println("Start rest gui client.");
+        showApplicationWindow();
+    }
+
+    private static void showApplicationWindow() {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Rest client");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JLabel label = new JLabel("Test label");
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(e -> label.setText("Submit button pressed"));
+
+        JPanel panel = new JPanel();
+        panel.add(label);
+        panel.add(submitButton);
+        frame.add(panel);
+        frame.setSize(300, 200);
+        positionToScreenCenter(frame);
+        frame.setVisible(true);
+    }
+
+    private static void positionToScreenCenter(JFrame frame) {
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        int x = (int) ((screen.getWidth() - frame.getWidth()) / 2);
+        int y = (int) ((screen.getHeight() - frame.getHeight()) / 2);
+        frame.setLocation(x, y);
     }
 }
